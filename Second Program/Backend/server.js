cda8c24b4825b085a1c0415fc135fb6f7ca02605
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-07-23 14:07:05 +0530

    made changes in the server.js file

--- Second Program/Backend/server.js
+++ Second Program/Backend/server.js
@@ -2,9 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send("server is ready")
-})
+// app.get('/', (req,res) => {
+//     res.send("server is ready")
+// })
 
 app.get('/jokes', (req,res) => {
     const jokes = [
