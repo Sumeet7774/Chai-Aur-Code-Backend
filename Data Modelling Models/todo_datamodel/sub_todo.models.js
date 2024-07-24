d7a60cbe849b074cba4a56772b581bf4e13dd67b
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-07-24 14:17:16 +0530

    created data model for todo app

--- Data Modelling Models/todo_datamodel/sub_todo.models.js
+++ Data Modelling Models/todo_datamodel/sub_todo.models.js
@@ -16,9 +16,6 @@ const subTodoSchema = new mongoose.Schema(
             type: mongoose.Schema.Types.ObjectId,
             ref: "User"
         }
-
-
-        
     }, {timestamps: true})
 
 export const SubTodo = mongoose.model("SubTodo", subTodoSchema)
